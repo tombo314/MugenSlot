dcba31b94d153b22d9022b301669941a8ac56d74
tombo314 <u_tombo@icloud.com>
2024-02-09 00:51:26 +0900

    Add roll.enhance(), choice---, buttonPrice---

--- public/main.js
+++ public/main.js
@@ -36,17 +36,23 @@ function enableButtonStart(choiceNum){
 class Roll{
     constructor(){
         this.idx;
+        this.choiceMoney = 10;
+        this.choiceMoneyIncrease = 10;
+        this.choiceSlotShorten = 5;
+        this.choiceSlotEnhance
         this.choice = [
             "所持金+10%",
             "所持金獲得量+10%",
-            "スロット間隔短縮30秒",
-            "所持金+30%",
-            "所持金獲得量+30%",
-            "スロット強化30秒",
-            "所持金+50%",
-            "オートスロット30秒",
+            "スロット間隔短縮5秒",
+            "スロット強化5秒",
+            "オートスロット5秒",
         ];
         this.choiceNum = this.choice.length;
+    }
+
+    enhance(rollStr){
+        if (rollStr==="所持金"){
+        }
     }
 
     setIdx(idx){
@@ -154,6 +160,7 @@ setInterval(() => {
     elemMoney.textContent = money;
     localStorage.setItem("money", money);
     localStorage.setItem("moneyIncrease", moneyIncrease);
+    updateButtonPriceState();
 }, 1000);
 
 // リセットボタン
@@ -181,35 +188,70 @@ function addStar(){
 // 強化
 let priceMoney = parseInt(localStorage.getItem("priceMoney"));
 let priceMoneyIncrease = parseInt(localStorage.getItem("priceMoneyIncrease"));
-let priceSlotEnforce = parseInt(localStorage.getItem("priceSlotEnforce"));
+let priceSlotEnhance = parseInt(localStorage.getItem("priceSlotEnhance"));
 let priceSlotShorten = parseInt(localStorage.getItem("priceSlotShorten"));
 let priceAutoSlot = parseInt(localStorage.getItem("priceAutoSlot"));
 
 if (isNaN(priceMoney)){
     priceMoney = 100;
     priceMoneyIncrease = 100;
-    priceSlotEnforce = 100;
+    priceSlotEnhance = 100;
     priceSlotShorten = 100;
     priceAutoSlot = 100;
     localStorage.setItem("priceMoney", 100);
     localStorage.setItem("priceMoneyIncrease", 100);
-    localStorage.setItem("priceSlotEnforce", 100);
+    localStorage.setItem("priceSlotEnhance", 100);
     localStorage.setItem("priceSlotShorten", 100);
     localStorage.setItem("priceAutoSlot", 100);
 }
 
 let elemPriceMoney = document.getElementById("js-price-money");
 let elemPriceMoneyIncrease = document.getElementById("js-price-money-increase");
-let elemPriceSlotEnforce = document.getElementById("js-price-slot-enforce");
+let elemPriceSlotEnhance = document.getElementById("js-price-slot-enhance");
 let elemPriceSlotShorten = document.getElementById("js-price-slot-shorten");
 let elemPriceAutoSlot = document.getElementById("js-price-auto-slot");
 
 elemPriceMoney.textContent = priceMoney;
 elemPriceMoneyIncrease.textContent = priceMoneyIncrease;
-elemPriceSlotEnforce.textContent = priceSlotEnforce;
+elemPriceSlotEnhance.textContent = priceSlotEnhance;
 elemPriceSlotShorten.textContent = priceSlotShorten;
 elemPriceAutoSlot.textContent = priceAutoSlot;
 
 if (money>=998244353){
     addStar();
 }
+
+let elemButtonPriceMoney = document.getElementById("js-button-price-money");
+let elemButtonPriceMoneyIncrease = document.getElementById("js-button-price-money-increase");
+let elemButtonPriceSlotEnhance = document.getElementById("js-button-price-slot-enhance");
+let elemButtonPriceSlotShorten = document.getElementById("js-button-price-slot-shorten");
+let elemButtonPriceAutoSlot = document.getElementById("js-button-price-auto-slot");
+
+function updateButtonPriceState(){
+    if (priceMoney<=money){
+        elemButtonPriceMoney.disabled = false;
+    } else {
+        elemButtonPriceMoney.disabled = true;
+    }
+    if (priceMoneyIncrease<=money){
+        elemButtonPriceMoneyIncrease.disabled = false;
+    } else {
+        elemButtonPriceMoneyIncrease.disabled = true;
+    }
+    if (priceSlotEnhance<=money){
+        elemButtonPriceSlotEnhance.disabled = false;
+    } else {
+        elemButtonPriceSlotEnhance.disabled = true;
+    }
+    if (priceSlotShorten<=money){
+        elemButtonPriceSlotShorten.disabled = false;
+    } else {
+        elemButtonPriceSlotShorten.disabled = true;
+    }
+    if (priceAutoSlot<=money){
+        elemButtonPriceAutoSlot.disabled = false;
+    } else {
+        elemButtonPriceAutoSlot.disabled = true;
+    }
+}
+updateButtonPriceState();
