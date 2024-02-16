839832253786a6839b693c2e1d1b0e899a51e2fd
tombo314 <u_tombo@icloud.com>
2024-02-17 02:08:38 +0900

    Update express -> http

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-let express = require("express");
-let server = express();
-server.use(express.static("public"));
+// let express = require("express");
+// let server = express();
+// server.use(express.static("public"));
+// server.listen(process.env.PORT || 8000);
+
+let http = require("http");
+let fs = require("fs");
+let server = http.createServer((req, res)=>{
+    if (req.url=="/"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(fs.readFileSync("index.html"));
+    } else if (req.url=="/stylesheet/main.css"){
+        res.writeHead(200, {"Content-Type": "text/css"});
+        res.end(fs.readFileSync("stylesheet/main.css"));
+    } else if (req.url=="/main.js"){
+        res.writeHead(200, {"Content-Type": "text/js"});
+        res.end(fs.readFileSync("main.js"));
+    }
+});
 server.listen(process.env.PORT || 8000);
